cfb393381643547dc94a25a5e54c1fceb3066bdc
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-07-03 21:15:36 -0300

    iniciando o projeto gerenciador de hábitos

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Dosis } from "next/font/google";
+import "./globals.css";
+
+const dosis = Dosis({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Gerenciador de Hábitos",
+  description: "Gerencie seus hábitos pelo seu celular",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={dosis.className}>{children}</body>
+    </html>
+  );
+}
